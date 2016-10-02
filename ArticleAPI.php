9f5fb9005a4cdd452e0c8b022bd2e35bcc14388d
Htoo Maung Thait <robinhtoo@gmail.com>
2016-10-02 21:15:55 +0630

    A Test Class with contructor and destructor is built in this file to test purpose. Previous working code is in commet below of  built class.

--- ArticleAPI.php
+++ ArticleAPI.php
@@ -1,50 +1,60 @@
 <?php
+
+class MyDestructableClass {
+   function __construct() {
+       print "In constructor\n";
+       $this->name = "MyDestructableClass";
+   }
+
+   function __destruct() {
+       print "Destroying " . $this->name . "\n";
+   }
+}
+
+$obj = new MyDestructableClass();
+
+
+/*
+
+$serverName = "localhost";
+$username ="root";
+$password = "";
+$db = "test"; 
+
+try {
+    $conn = new PDO("mysql:host=$serverName;dbname=$db", $username, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     
-    $serverName = "localhost";
-    $username ="root";
-    $password = "";
-    $db = "test"; 
+    //set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully";
 
-    try {
-        $conn = new PDO("mysql:host=$serverName;dbname=$db", $username, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-        //set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo "Connected successfully";
+    //Transaction Start
+    $conn->beginTransaction();
+    $stmt = $conn->prepare("SELECT * FROM tbl_article;");
+    $stmt->execute();
 
-        $conn->beginTransaction();
-        $stmt = $conn->prepare("SELECT * FROM tbl_article;");
-        $stmt->execute();
-
-        // set the resulting array to associative
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC); 
-        $stmt->closeCursor();
-
-        /*header('Content-type: text/html; charset=utf-8');    
-        print_r($result);*/
-        //var_dump($result);
-
-        /*header('Content-type: text/html; charset=utf-8');
-        $i = 0;
-        foreach ($result as $row) {
-            echo ++$i;
-            echo $row['article_title']."<br/>";
-            echo "str length:".mb_strlen($row['article_title'])."<br/>";
-            
-
-        }*/
-
-        header('Content-Type: application/json; charset=utf-8');
-        echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
-        //echo json_encode($result);
-
-        $conn->commit();
-
-
-    } catch (PDOException $pdoe) {
-        $conn->rollBack();
-        echo "Connection failed: ". $pdoe->getMessage();
-    }
+    // set the resulting array to associative
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+    $stmt->closeCursor();
 
     
+    //JSON Production Like API
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
     
 
+    $conn->commit();
+
+
+} catch (PDOException $pdoe) {
+    $conn->rollBack();
+    echo "Connection failed: ". $pdoe->getMessage();
+}    
+        
+    
+    
+
+    
+    
+
+*/
